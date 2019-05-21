59562cf44b904f4d2960b35ce3de123603cdca89
Nisch <nisch@Nischs-MacBook-Pro.local>
2019-05-20 18:17:22 -0600

    Added basic import button-and-command structure for importing buttons and commands into the UI and tested basic exec against our hardware platform

--- sprenderer.js
+++ sprenderer.js
@@ -11,6 +11,7 @@ var ftdi = require('ftdi');
 const electron = require('electron');
 const {ipcRenderer} = electron;
 
+// TODO -- static --> and then --> callback structure for correct data destination
 
 
 SerialPort.list((err, ports) => {
@@ -51,9 +52,17 @@ SerialPort.list((err, ports) => {
 // Aliasing seems to be not working
 // Direct use of 3,000,000 looks ok - directly as a call
 // But can't call with a higher value, and aliasing seems to do nothing
-/*var serialOpenByName = function (name) {
+var serialOpenByName = function (name) {
 
-  port = new SerialPort(name, { autoOpen: false, baudRate: 4000000 }, function (err) {
+  //port = new SerialPort(name, { autoOpen: false, baudRate: 3000000 }, function (err) {
+  var settings = {
+    autoOpen: false,
+    baudRate: 57600, //921600, //57600,
+    databits: 8,
+    stopbits: 1,
+    parity  : 'none',
+  };
+  port = new SerialPort(name, settings, function (err) {
     if ( err ) {
       return console.log('sprenderer: error on create new: ', err.message)
     }
@@ -61,19 +70,43 @@ SerialPort.list((err, ports) => {
   port.on('open', function() {
     console.log('port.on open');
   });
+  //port.on('data', function (data) {
+  //  console.log('Data: ', data);
+  //});
   port.on('data', function (data) {
-    console.log('Data: ', data);
-  })
+    console.log ("port.on data");
+    //if ( data.length > 3000 ) {  // just testing - so would like to see a large sample
+    if ( !filling ) {
+      setTimeout(function() {
+        mainWindowUpdateChartData(buf);
+        buf = [];
+        filling = false;
+      }, 2000);
+      filling = true;
+    }
+    if ( filling ) {
+      data.forEach(function(d) {
+        buf.push(d);
+      });
+    }
+
+    //ipcRenderer.send('port:ondata', data);
+    //console.log('Data: (length): ', data.length);
+    console.log(data);
+    console.log("Same data parsed as ASCII chars: ");
+    console.log(hexBufToAscii(data));
+    //}
+  });
   port.open()
-}*/
+}
 
 var buf = [];
 var nsamp = 4096;
 var filling = false;
 
-var serialOpenByName = function (name) {
+/*var serialOpenByName = function (name) {
 
-  port = new ftdi.FtdiDevice(0);
+  port = new ftdi.FtdiDevice(0); // for d2xx only
 
   //ftdi.find( function(err, devices) {
   //  port = new ftdi.FtdiDevice(devices[0]);
@@ -85,6 +118,7 @@ var serialOpenByName = function (name) {
       console.log('port.on open');
     });
     port.on('data', function (data) {
+      console.log ("port.on data");
       //if ( data.length > 3000 ) {  // just testing - so would like to see a large sample
       if ( !filling ) {
         setTimeout(function() {
@@ -120,21 +154,36 @@ var serialOpenByName = function (name) {
     // can return without error such that program execution continues -
     // because using this with eg async 245 fifo mode doesn't require that information
     // and futher that information has no meaning - so we only need the open call
-    port.open({
-      /*baudrate: 12000000,
+    var settings = {
+      baudrate: 57600,
       databits: 8,
       stopbits: 1,
-      parity: 'none',
-      */
-      // bitmode: 'cbus', // for bit bang
-      // bitmask: 0xff    // for bit bang
+      parity  : 'none',
+    };
+    port.open({ //{
+      //baudrate: 12000000,
+      //databits: 8,
+      //stopbits: 1,
+      //parity: 'none',
+      //// bitmode: 'cbus', // for bit bang
+      //// bitmask: 0xff    // for bit bang
+      settings
     });
+
 
   //});
 }
+*/
 
-
-
+// TODO move to utils
+// https://www.w3resource.com/javascript-exercises/javascript-string-exercise-28.php
+function hexBufToAscii (dataAsHexBuf) {
+	var str = '';
+	for (var n = 0; n < dataAsHexBuf.length; n++) {
+		str += String.fromCharCode(parseInt(dataAsHexBuf[n]));
+	}
+	return str;
+}
 
 var serialClose = function () {
   if ( port ) port.close ( function (err) {
@@ -147,13 +196,16 @@ var serialClose = function () {
 
 var serialTestWrite = function () {
   if ( port ) {
-    var buffer = new Buffer(10);
+    /*var size = 10;
+    var buffer = new Buffer(size);
     var i = 0;
-    for (i=0; i<10; i++) {
+    for (i=0; i<size; i++) {
       buffer[i] = 0xaa;
-    }
+    }*/
+    var cmd = [ 0x53, 0xC4, 0x01, 0x00, 0x50 ];
     //port.write('Testing123', function (err) {
-    port.write(buffer, function (err) {
+    //port.write(buf, function (err) {
+    port.write(cmd, function (err) {
       if ( err ) {
         console.log('sprenderer: error on write: ', err.message)
       }
@@ -161,9 +213,45 @@ var serialTestWrite = function () {
   }
 }
 
+var serialSendData = function ( commandAndType, returnDataTo) {
+  if ( port ) {
+    var cmdarr = [];
+    // Warning: there needs of course to be some kind of limits and sanity
+    // checking structure -- length of commands, etc.
+    // This could be a separate structure that is required with an imported
+    // command set such that validation occurs within this ruleset prior to
+    // allowing execution
+    switch ( commandAndType.type ) {
+
+      case "hexCsvBytes":
+        var cmd = commandAndType.value.replace(/\s/g,"").split(',');
+        var cmdarr = [];
+        cmd.forEach(function(c) {
+          cmdarr.push(parseInt(c));
+        });
+        console.log(cmdarr);
+        break;
+
+      default:
+        console.log('sprenderer: error on serialSendData: type in command with type is not (yet) supported: ' + commandAndType.type);
+    }
+
+    port.write(cmdarr, function (err) {
+      if ( err ) {
+        console.log('sprenderer: error on write within serialSendData: ', err.message)
+      }
+    });
+
+  } else {  // if not port ...
+    console.log ('sprenderer: error on serialSendData: port does not yet exist or has not been opened');
+  }
+
+}
+
 
 module.exports = {
   serialOpenByName: serialOpenByName,
   serialClose: serialClose,
   serialTestWrite: serialTestWrite,
+  serialSendData: serialSendData,
 };
