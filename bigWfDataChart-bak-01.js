2c5a699271d8d00b4dfd30a14b901f78feb93041
Nisch <nks@oshablue.com>
2019-12-21 15:17:59 -0700

    Interim commit.  Chart works reasonably and basically well as a responsive svg and also with reasonably limited and functional zoom and pan.  Draft.

--- bigWfDataChart-bak-01.js
+++ bigWfDataChart-bak-01.js
@@ -1,5 +1,3 @@
-// References (oh, so many):
-//
 // Lovely tutorial:
 // https://www.dashingd3js.com/creating-svg-elements-based-on-data
 // Additional lovely references:
@@ -18,16 +16,18 @@
 // https://github.com/samcday/node-stream-buffer
 // https://nodejs.org/api/stream.html
 // https://www.npmjs.com/package/stream-buffers
-//
-// Excellent d3 zoom how-to type of manual along with svg vs canvas (or mixed)
-// https://www.freecodecamp.org/news/get-ready-to-zoom-and-pan-like-a-pro-after-reading-this-in-depth-tutorial-5d963b0a153e/
+
+
+
 
 
 
 var chartContainer = document.getElementById('chart');
 
 
+var zoom = d3.zoom().scaleExtent([1, 100]);
 
+// Typically uses window.innerHeight / .innerWidth
 var chartHeight = 300
 var chartWidth = 900
 var _margin = 20
@@ -35,108 +35,111 @@ var margin = {top: _margin, right: _margin, bottom: _margin, left: _margin}
   , width = chartWidth - margin.left - margin.right // Use the window's width
   , height = chartHeight - margin.top - margin.bottom; // Use the window's height
 
-
-
-// Please See:
-// https://stackoverflow.com/questions/57007378/d3-zoom-and-drag-with-svg-axes-and-canvas-chart
-// As to why we are specifying all three of these below:
-var zoom = d3.zoom()
-  .scaleExtent([1, 10])
-  // left, top ... right, bottom
-  .translateExtent([[0,0],[chartWidth, chartHeight]]) // Unfortunately, this is dependent on window sizing, so would need to grab real-world size data
-  .extent([[0,0],[chartWidth, chartHeight]]) // operates totally differently
-  ;
-
-
-
 // The number of datapoints
 var n = 4095; //4096 //5000; //2500; //5000;
 
-//
+// 5. X scale will use the index of our data
 var xScale = d3.scaleLinear()
     .domain([0, n-1]) // input
     .range([0, width]); // output
 
-//
+// 6. Y scale will use the randomly generate number
 var yInputMaxVal = 255;
 var yScale = d3.scaleLinear()
     .domain([0, yInputMaxVal]) // input
     .range([height, 0]); // output
 
-// d3 line generator
+// 7. d3's line generator
 var line = d3.line()
     .x(function(d, i) { return xScale(i); }) // set the x values for the line generator
     .y(function(d) { return yScale(d); }) // yScale(d.y); }) // set the y values for the line generator
     .curve(d3.curveMonotoneX) // apply smoothing to the line
 
-// TODO - we should add some high freq low amp overlay to test semantic zoom
-// over geometric zoom ...
-var dataset = [];
+// 8. An array of objects of length N. Each object has key -> value pair, the key being "y" and the value is a random number
+var dataset = []; //= [0x00,0x01,0x03,0x07,0x0f,0x1f,0x3f,0x7f,0xff]; // d3.range(n).map(function(d) { return {"y": d3.randomUniform(1)() } })
 var j;
 for ( j = 0; j < 4095; j++ ) {
   dataset.push(127 * Math.sin(2*3.14159/4095*j*4) + 127);
 }
 
-
-
+// 1. Add the SVG to the page and employ #2
+/*
+var svg = d3.select('#chart').append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    */
+/*var zoom = d3.zoom()
+  .x(x)
+  .y(y)
+  .on("zoom", zoomed);*/
 
 var svg = d3.select('#chart')
   .append("div")
   .classed("svg-container", true)
   .append("svg")
-  .attr("preserveAspectRatio", "xMinYMin meet")
-  .attr("viewBox", "0 0 " + chartWidth + " " + chartHeight)
-  .classed("svg-content-responsive", true)
   .call(zoom.on("zoom", zoomed))
   .on("dblclick.zoom", null)          // cancels double-clicking to zoom
   .on("dblclick", ourDlbClick)
+  // Responsive SVG needs these 2 attributes and no width and height attr.
+ .attr("preserveAspectRatio", "xMinYMin meet")
+ .attr("viewBox", "0 0 " + chartWidth + " " + chartHeight)
+ // Class to make it responsive.
+ .classed("svg-content-responsive", true)
+ // This, below, works, but does not redraw and lines get thicker etc.
+ //.call(d3.zoom().on("zoom", function () {
+ //svg.attr("transform", d3.event.transform)
 
-  .append("svg")
-  .attr("width", chartWidth) //width + margin.left + margin.right)
-  .attr("height", chartHeight) //height + margin.top + margin.bottom)
+ //}))
+ .append("svg")
 
-  .append("g")
-  .classed("chartBody", true)
-  .attr("transform", "translate(" + (margin.left + margin.right) + "," + 0 + ")")
-  ;
+ .attr("width", chartWidth) //width + margin.left + margin.right)
+ .attr("height", chartHeight) //height + margin.top + margin.bottom)
+ //.call(zoom)
 
+ .append("g")
+ .classed("chartBody", true)
+ //.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+ //.attr("transform", "translate(" + (margin.left + margin.right) + "," + (margin.bottom + margin.top) + ")");
+ .attr("transform", "translate(" + (margin.left + margin.right) + "," + 0 + ")")
 
-// Trying above responsive svg, per:
+ ;
+// Trying instead of above, responsive svg, per:
 // https://stackoverflow.com/questions/16265123/resize-svg-when-window-is-resized-in-d3-js
-// as well as
-// http://thenewcode.com/744/Make-SVG-Responsive
 
 
-// X-axis in a group tag
+// 3. Call the x axis in a group tag
 svg.append("g")
     .attr("class", "x axis")
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(xScale)); // Create an axis component with d3.axisBottom
 
-// Y-axis in a group tag
+// 4. Call the y axis in a group tag
 svg.append("g")
     .attr("class", "y axis")
     .call(d3.axisLeft(yScale)); // Create an axis component with d3.axisLeft
 
-// Data line in a path tag
+// 9. Append the path, bind the data, and call the line generator
 svg.append("path")
+    //.data(dataset) // 10. Binds data to the line // was datum()
     .datum(dataset)
-    .attr("class", "line")
-    .attr("d", line);
+    .attr("class", "line") // Assign a class for styling
+    .attr("d", line); // 11. Calls the line generator
 
-
+// 12. Appends a circle for each datapoint
+/*svg.selectAll(".dot")
+    .data(dataset)
+    .enter().append("circle") // Uses the enter().append() method
+    .attr("class", "dot") // Assign a class for styling
+    .attr("cx", function(d, i) { return xScale(i) })
+    .attr("cy", function(d) { return yScale(d) }) //.y) })
+    .attr("r", 3);*/
 
 
 
 function ourDlbClick() {
-  svg.transition().duration(500).call(zoom.transform, d3.zoomIdentity.scale(1));
-  //.translate(margin.left + margin.right,0)); // already done in the zoomed() now
-
-  // TODO BUG there is still a bug where after reset, the next (scroll wheel at least)
-  // attempt to zoom snaps right back to the last transform/zoom and thus doesn't make
-  // any UI sense ...
-  // Probably just add some capture and assign?
-
+  svg.transition().duration(500).call(zoom.transform, d3.zoomIdentity.scale(1).translate(margin.left + margin.right,0));
 
 }
 
@@ -147,17 +150,23 @@ function zoomed() {
   console.log(d3.event.transform);    // v5
   var currentTransform = d3.event.transform;
 
-  // Because we translate the chart above, we should do:
-  currentTransform = currentTransform.translate(margin.left + margin.right,0);
-
   svg.attr("transform", currentTransform);
-  d3.axisBottom().scale(currentTransform.rescaleX(xScale));
-  d3.axisLeft().scale(currentTransform.rescaleY(yScale));
+  d3.axisBottom().scale(d3.event.transform.rescaleX(xScale));
+  d3.axisLeft().scale(d3.event.transform.rescaleY(yScale));
 
-  //
-  // TODO - move now from geometric zoom to semantic zoom
-  //
 
+
+
+
+  /*svg.select(".x.axis").call(xAxis);
+  svg.select(".y.axis").call(yAxis);
+  svg.select(".line")
+      .attr("class", "line")
+      .attr("d", line);*/
+
+      // Works, but just zooms into the svg, thus eg thicker lines,
+      // not good for actual data view zooming
+      //svg.attr("transform", d3.event.transform);
 }
 
 
