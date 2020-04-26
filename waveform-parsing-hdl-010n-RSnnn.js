7ea5e441575eefb1713040d602379527fc2644a8
OshaBlue <everyone@oshablue.com>
2020-04-26 14:52:51 -0600

    [Interim] Custom requested file output batch capture format (UTR) loads correctly in the legacy software proofing version.  Algorithm breaks due to conversion errors (in the legacy software) that create massive arrays with a tiny conversion error.  So to actually use the UTR file output in the legacy software, use a 25000000 sample rate setting in mainWindow.

--- waveform-parsing-hdl-010n-RSnnn.js
+++ waveform-parsing-hdl-010n-RSnnn.js
@@ -1,4 +1,9 @@
 
+
+// Constants
+//
+//
+
 const fullWfSizeInclSof = 4095;
 const sof = [0xaa, 0x55, 0xaa, 0x55, 0x00, 0x00, 0x00, 0x00, 0xff, 0x00, 0xff, 0x00];
 const channelNumIndex = 4;
@@ -10,8 +15,28 @@ const sofSkipMatchByte = [ 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0];
 const lengthOfSof = sof.length;
 const warningThresholdBufMult = 100;
 const chanAddedBaseAtWhichToIncrementScanNum = 1;
+const warnMaxHardwareChannelBase1 = 8;
 
-var findSofPair = ( buf, startIndex, startingScanNum ) => {
+// This is the HDL-0108-RSCPT default scan rate
+// Each channel is intended to grab 16 WFs per second
+// So, 16 x 8 channels = 128 Hz new waveform/sec rate
+// Even if only 4 of 8 channels are put to file,
+// this is the overall scan rate, so the time to complete
+// e.g. a nominal 150 scans per file is based on the same 128 Hz
+// This can be used to setup a progress bar in the UI
+const nominalWaveformReceiveFrequencyHz = 128;
+
+// HDL-0108-RSCPT Nominal ADC voltage scaling
+const hdlSlope = (1.024 / 255.0);
+const hdlOffset = -0.512;
+
+//
+//
+// End of constants
+
+
+
+var findSofPair = ( buf, startIndex, startingScanNum, maxScanNumToInclude ) => {
 
   // Return: {
   //   index of first byte of first start of frame,
@@ -34,8 +59,13 @@ var findSofPair = ( buf, startIndex, startingScanNum ) => {
     //console.log(sofStartSeq);
 
     var matchIndex = buf.indexOf(sofStartSeq, startIndex);
-    var sofStopSeqMatchIndex = buf.indexOf(sofStopSeq, startIndex);
-    //console.log("matchIndex: " + matchIndex + " for startIndex: " + startIndex);
+
+    // there could be a sofStopSeq (for example if the parent function to only
+    // stream data starting at the first SOF isn't working or etc.)
+    // so offset by the first match index for the start
+    var sofStopSeqMatchIndex = buf.indexOf(sofStopSeq, matchIndex); //startIndex);
+    console.log("matchIndex: " + matchIndex + " for startIndex: " + startIndex);
+    console.log("sofStopSeqMatchIndex: " + sofStopSeqMatchIndex + " for startIndex: " + startIndex);
 
     if (
       matchIndex > -1
@@ -43,23 +73,51 @@ var findSofPair = ( buf, startIndex, startingScanNum ) => {
       && ( sofStopSeqMatchIndex - matchIndex == sofDeltaStartToStop)
     ) {
 
+      console.log(`raw chan byte: ${buf[matchIndex + channelNumIndex]}`);
+      let s = buf.slice(matchIndex, 12); //sofSize);
+      console.log(s);
+
       var nextStartIndex = matchIndex + sofSize;
       var matchIndex2 = buf.indexOf(sofStartSeq, nextStartIndex);
-      var sofStopSeqMatchIndex2 = buf.indexOf(sofStopSeq, nextStartIndex);
+      var sofStopSeqMatchIndex2 = buf.indexOf(sofStopSeq, matchIndex2); //nextStartIndex);
+
+      console.log("matchIndex2: " + matchIndex2 + " for nextStartIndex: " + nextStartIndex);
+      console.log("sofStopSeqMatchIndex2: " + sofStopSeqMatchIndex2 + " for nextStartIndex: " + nextStartIndex);
+
+      //console.log(`raw chan byte: ${buf[matchIndex + channelNumIndex]}`);
+      //console.log(buf.slice(matchIndex, sofSize));
 
       if (
         matchIndex2 > -1
         && sofStopSeqMatchIndex2 > -1
         && ( sofStopSeqMatchIndex2 - matchIndex2 == sofDeltaStartToStop)
       ) {
-        console.log(`raw chan byte: ${buf[matchIndex + channelNumIndex]}`);
-        console.log(buf.slice(matchIndex, sofSize));
+        //console.log(`raw chan byte: ${buf[matchIndex + channelNumIndex]}`);
+        //console.log(buf.slice(matchIndex, sofSize));
 
         var chan = buf[matchIndex + channelNumIndex] + channelNumIndexBaseAddend;
 
+        if ( chan > warnMaxHardwareChannelBase1 ) {
+          console.warn(`Warning: waveform-parseing-hdl...js: a channel number, ${chan}, larger than the hardware maximum channel number, ${warnMaxHardwareChannelBase1}, was encountered.  This is likely an intrahardware signaling or configuration issue and should be debugged, culminating in a firmware update.`);
+        }
+
         var scan = chan == chanAddedBaseAtWhichToIncrementScanNum ? startingScanNum + 1 : startingScanNum;
-        //console.log("matchIndex2: " + matchIndex2 + " for next startIndex: " + nextStartIndex);
-        return { sof1: matchIndex, sof2: matchIndex2, chan: chan, scan: scan };
+        console.log("matchIndex2: " + matchIndex2 + " for next startIndex: " + nextStartIndex);
+
+        // Now make sure we're not beyond the end of the desired scan capture length
+        // If we don't want to lose input data buffer information,
+        // the exclude high scan numbers here.
+        // If we do want to cut out unused waveforms, like non-channel 1 waveforms
+        // in a new file prior to receiving the first channel 1 waveform,
+        // then return ok here and exclude in the calling/parent function levels
+        // such that that particular data is removed from the parent/full input
+        // data buffer
+        if ( scan > maxScanNumToInclude ) {
+            // this will go to the function's last return statement of nothing/null
+            console.log(`scan number ${scan} is greater than max to include - not returning this sof set`);
+        } else {
+          return { sof1: matchIndex, sof2: matchIndex2, chan: chan, scan: scan };
+        }
       }
     }
 
@@ -88,7 +146,7 @@ var findSofPair = ( buf, startIndex, startingScanNum ) => {
 
 
 
-var extractSofBoundsSets = ( rawDataBuffer, startingScanNum ) => {
+var extractSofBoundsSets = ( rawDataBuffer, startingScanNum, maxScanNumToInclude ) => {
 
   let diArr = [];
 
@@ -97,7 +155,7 @@ var extractSofBoundsSets = ( rawDataBuffer, startingScanNum ) => {
   var stillGood = true;
   var startIndex = 0;
   while ( stillGood ) {
-    var res = findSofPair(rawDataBuffer, startIndex, startingScanNum);
+    var res = findSofPair(rawDataBuffer, startIndex, startingScanNum, maxScanNumToInclude );
     if ( res ) {
       diArr.push(res);
       startIndex = res.sof2;
@@ -117,8 +175,6 @@ var extractSofBoundsSets = ( rawDataBuffer, startingScanNum ) => {
 
 
 
-const hdlSlope = (1.024 / 255.0);
-const hdlOffset = -0.512;
 
  var valToScaledFloat = ( val ) => {
    //console.log(val);
@@ -135,7 +191,12 @@ const hdlOffset = -0.512;
 
 
 module.exports = {
-  extractSofBoundsSets,
+
+  // Constants
   lengthOfSof,
+  nominalWaveformReceiveFrequencyHz,
+
+  // Functions
+  extractSofBoundsSets,
   valToScaledFloat
 };
